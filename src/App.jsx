1dbdc5cb2d799e7a6ef480177215dc5109d6136d
Silverio Huarcaya <corwilgi@gmail.com>
2022-09-21 17:27:09 -0500

    config initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Silverio Huarcaya</h1>
+    </header>
+  </div>
+)
+
+export default App
