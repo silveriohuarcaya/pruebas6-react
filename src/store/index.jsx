10bb42a3f5ff420220121f9efafe8a315d2ed6ba
Silverio Huarcaya <corwilgi@gmail.com>
2022-10-24 11:08:44 -0500

    feature:shopping cart

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable indent */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable prettier/prettier */
 import { createContext, useReducer, useContext } from 'react';
 
 const AppContext = createContext();
@@ -17,12 +20,54 @@ function reduce(state, action) {
         ...state,
         products: action.payload,
       };
-    case 'ADD_TO_CART':
+    case 'ADD_TO_CART': {
+      const itemInCart = state.cart.find((item) => item._id === action.payload._id);
+      return itemInCart
+        ? {
+          ...state,
+          cart: state.cart.map(
+            // eslint-disable-next-line no-confusing-arrow
+            (item) => item._id === action.payload._id
+            // eslint-disable-next-line operator-linebreak
+            ?
+             {...item, quantity: item.quantity + 1 }
+            : item,
+            ),
+          total: state.total + action.payload.price,
+          }
+        : {
+          ...state,
+          cart: [...state.cart, { ...action.payload, quantity: 1 }],
+          total: state.total + action.payload.price,
+        };
+    }
+    case 'DELETE_TO_CART':
       return {
         ...state,
-        cart: [...state.cart, action.payload],
-        total: state.total + action.payload.price,
+        cart: action.payload,
+        total: 0,
       };
+      case 'DELETE_ONE_TO_CART': {
+        return action.payload.quantity > 1
+        ? {
+          ...state,
+          cart: state.cart.map((item) => (item._id === action.payload._id
+             ? {...item, quantity: item.quantity - 1} : item)),
+          total: state.total - action.payload.price,
+        }
+        : {
+          ...state,
+          cart: state.cart.filter((item) => (item._id !== action.payload._id)),
+          total: state.total - action.payload.price,
+        };
+      }
+      case 'DELETE_ALL_TO_CART': {
+        return {
+          ...state,
+          cart: state.cart.filter((item) => item._id !== action.payload._id),
+          total: state.total - (action.payload.price * action.payload.quantity),
+        };
+      }
     default:
       return state;
   }
