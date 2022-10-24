159cd31f22642037ecc53873bb4c4a7043d88a6c
silveriohuarcaya <corwilgi@gmail.com>
2022-10-24 11:44:11 -0500

    Develop (#21)
    * fix: register
    
    * fix nodemailer
    
    * feat: register
    
    * feature: register
    
    * feat: fix register
    
    * feat: register
    
    * fix: register (#2)
    
    * feature: payment
    
    * Delete gibogibo
    
    * fix: payment
    
    * fix: register (#2)
    
    * feature: login-register-sengrid
    
    * feat: register
    
    * feat: fix register
    
    * fix: register (#2)
    
    * fix: register (#2)
    
    * Develop (#4)
    
    * fix: register
    
    * fix nodemailer
    
    * feat: register
    
    * feature: register
    
    * feat: fix register
    
    * feat: register
    
    * fix: register (#2)
    
    * feature: payment
    
    * Delete gibogibo
    
    * fix: payment
    
    * fix: register (#2)
    
    * fix: register (#2)
    
    * fix: register (#2)
    
    * fix: login-register-sengrid (#7)
    
    * fix: login-register-sengrid (#9)
    
    * Update index.scss
    
    * Update index.scss
    
    * Feature/t1 (#12)
    
    * fix: url
    
    * Update index.scss
    
    * Feature/t1 (#14)
    
    * fix: url
    
    * Update index.scss
    
    * mercadopago
    
    * Feature/t1 (#16)
    
    * fix: url
    
    * Update index.scss
    
    * mercadopago
    
    * feature: fix
    
    * feat:fix
    
    * Feature/t1 (#18)
    
    * fix: url
    
    * Update index.scss
    
    * mercadopago
    
    * feature: fix
    
    * feat:fix
    
    * feature:upload
    
    * Feature/t1 (#20)
    
    * fix: url
    
    * Update index.scss
    
    * mercadopago
    
    * feature: fix
    
    * feat:fix
    
    * feature:upload
    
    * feature:shopping cart

--- src/components/Products/Badge.jsx
+++ src/components/Products/Badge.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-return-assign */
 import { useAppContext } from '../../store';
 
 const Badge = () => {
   const { state } = useAppContext();
-  return <span className="badge">{state.cart.length}</span>;
+  let quantity = 0;
+  state.cart.map((product) => (quantity += product.quantity));
+  return <span className="badge">{quantity}</span>;
 };
 
 export default Badge;
--- src/components/Products/ItemCart.jsx
+++ src/components/Products/ItemCart.jsx
@@ -1,15 +1,66 @@
+import { useAppContext } from '../../store';
+
 const ItemCart = (props) => {
   const { item } = props;
   const { name, price, quantity, img } = item;
+  const { dispatch } = useAppContext();
+
+  const handleClickIncrement = () => {
+    const action = {
+      type: 'ADD_TO_CART',
+      payload: item,
+    };
+    dispatch(action);
+  };
+
+  const handleClickDecrement = () => {
+    const action = {
+      type: 'DELETE_ONE_TO_CART',
+      payload: item,
+    };
+    dispatch(action);
+  };
+
+  const handleClickDelete = () => {
+    const action = {
+      type: 'DELETE_ALL_TO_CART',
+      payload: item,
+    };
+    dispatch(action);
+  };
+
   return (
     <li className="clearfix">
-      <img src={img} alt={`product ${name}`} width="40" height="40" />
-      <span className="item-name">{name}</span>
-      <span className="item-price">${price}</span>
-      <span className="item-quantity">
-        Quantity:
-        {quantity}
-      </span>
+      <div className="div-delete-img">
+        <div className="div-delete">
+          <button className="cart-button" type="button" onClick={handleClickDelete}>
+            x
+          </button>
+        </div>
+        <div className="div-img">
+          <img src={img} alt={`product ${name}`} width="40" height="40" />
+        </div>
+        <div className="div-name">
+          <span className="item-name">{name}</span>
+        </div>
+      </div>
+      <div className="div-price-quantity">
+        <div className="item-price">
+          <span>${Intl.NumberFormat('en-US').format(price * quantity)}</span>
+        </div>
+        <div className="item-quantity">
+          <span>
+            Quantity:
+            <b>{quantity}</b>
+          </span>
+          <button className="cart-button-quantity" type="button" onClick={handleClickIncrement}>
+            +
+          </button>
+          <button className="cart-button-quantity" onClick={handleClickDecrement} type="button">
+            -
+          </button>
+        </div>
+      </div>
     </li>
   );
 };
